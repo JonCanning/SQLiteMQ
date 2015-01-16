86087c23785e84f606962dc4b7ee6cea000c6a39
Jon Canning <jon.canning@gmail.com>
2015-01-16 16:21:18 +0000

    manual nuget package

--- build.fsx
+++ build.fsx
@@ -17,8 +17,24 @@ Target "Test" (fun _ ->
   testDlls |> NUnit(fun p -> 
                 { p with DisableShadowCopy = true
                          OutputFile = "TestResults.xml" }))
+Target "NuGet" (fun _ -> 
+  (new System.Net.WebClient()).DownloadFile("http://nuget.org/nuget.exe", @"nuget.exe")
+  directExec 
+    (fun psi -> 
+    psi.FileName <- "nuget.exe"
+    psi.Arguments <- sprintf "pack src\SQLiteMQ\SQLiteMQ.fsproj -Version %s -Prop Configuration=Release" 
+                       appVeyorBuildVersion)
+  |> ignore
+  directExec 
+    (fun psi -> 
+    psi.FileName <- "nuget.exe"
+    psi.Arguments <- sprintf "push SQLiteMQ.%s.nupkg -ApiKey %s -Source %s" appVeyorBuildVersion 
+                     <| environVar "NUGETAPIKEY" <| environVar "NUGETSOURCE")
+  |> ignore)
 Target "Default" (fun _ -> trace "Building and Running Tests")
+Target "Appveyor" DoNothing
 // Dependencies
 "Build" ==> "Test" ==> "Default"
+"Build" ==> "Test" ==> "Nuget" ==> "Appveyor"
 // start build
 RunTargetOrDefault "Default"
